0d72df4eb289c4e7de8a053728dfa26919ab127f
ㅇi쀼z1 <yaewon0411@naver.com>
2024-11-25 14:29:50 +0900

    feat(genre-controller): Spotify 제공 장르 목록 저장 api 추가 #10

--- src/main/java/com/my/firstbeat/web/controller/genre/GenreController.java
+++ src/main/java/com/my/firstbeat/web/controller/genre/GenreController.java
@@ -1,12 +1,16 @@
 package com.my.firstbeat.web.controller.genre;
 
 import com.my.firstbeat.client.spotify.dto.response.TrackSearchResponse;
+import com.my.firstbeat.web.service.GenreService;
 import com.my.firstbeat.web.service.TrackService;
 import com.my.firstbeat.web.util.api.ApiResult;
+import jakarta.validation.constraints.Positive;
+import jakarta.validation.constraints.PositiveOrZero;
 import lombok.RequiredArgsConstructor;
 import org.springframework.http.ResponseEntity;
 import org.springframework.validation.annotation.Validated;
 import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.PostMapping;
 import org.springframework.web.bind.annotation.RequestParam;
 import org.springframework.web.bind.annotation.RestController;
 
@@ -16,11 +20,19 @@ import org.springframework.web.bind.annotation.RestController;
 public class GenreController {
 
     private final TrackService trackService;
+    private final GenreService genreService;
 
-    @GetMapping("/auth/search/track") //임시 api
+    @GetMapping("/auth/search/tracks") //임시 api
     public ResponseEntity<ApiResult<TrackSearchResponse>> searchTrackList(@RequestParam(value = "genre", required = true) String genre,
-                                                                          @RequestParam(value = "page", required = false, defaultValue = "0") Long page,
-                                                                          @RequestParam(value = "limit", required = false, defaultValue = "20")  Long limit){
+                                                                          @RequestParam(value = "page", required = false, defaultValue = "0") @PositiveOrZero Long page,
+                                                                          @RequestParam(value = "limit", required = false, defaultValue = "20") @Positive Long limit){
         return ResponseEntity.ok(ApiResult.success(trackService.searchTrackList(genre)));
     }
+
+    @PostMapping("/api/v1/genres")
+    public ResponseEntity<ApiResult<String>> getAndUpdateGenreList(){
+        genreService.updateGenreList();
+        return ResponseEntity.ok(ApiResult.success("Spotify에서 제공하는 장르 목록 저장 완료"));
+    }
+
 }
