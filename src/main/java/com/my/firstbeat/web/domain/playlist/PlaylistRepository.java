36336f7be4d8ba70846cc8b2ef1f94bc195bbba2
ㅇi쀼z1 <yaewon0411@naver.com>
2024-11-25 19:05:25 +0900

    feat(repo): 트랙 추천 기능에 사용할 조회 메서드 추가 #9

--- src/main/java/com/my/firstbeat/web/domain/playlist/PlaylistRepository.java
+++ src/main/java/com/my/firstbeat/web/domain/playlist/PlaylistRepository.java
@@ -1,6 +1,19 @@
 package com.my.firstbeat.web.domain.playlist;
 
+import com.my.firstbeat.web.domain.track.Track;
+import com.my.firstbeat.web.domain.user.User;
 import org.springframework.data.jpa.repository.JpaRepository;
+import org.springframework.data.jpa.repository.Query;
+import org.springframework.data.repository.query.Param;
+import org.springframework.security.core.parameters.P;
+
+import java.util.List;
 
 public interface PlaylistRepository extends JpaRepository<Playlist, Long> {
+
+    @Query("select t from Playlist p " +
+            "left join PlaylistTrack pt on pt.playlist = p " +
+            "left join Track t on pt.track = t " +
+            "where p.user = :user")
+    List<Track> findAllTrackByUser (@Param("user") User user);
 }
--- src/main/java/com/my/firstbeat/web/domain/userGenre/UserGenreRepository.java
+++ src/main/java/com/my/firstbeat/web/domain/userGenre/UserGenreRepository.java
@@ -1,6 +1,15 @@
 package com.my.firstbeat.web.domain.userGenre;
 
+import com.my.firstbeat.web.domain.genre.Genre;
+import com.my.firstbeat.web.domain.user.User;
 import org.springframework.data.jpa.repository.JpaRepository;
+import org.springframework.data.jpa.repository.Query;
+import org.springframework.data.repository.query.Param;
+
+import java.util.List;
 
 public interface UserGenreRepository extends JpaRepository<UserGenre, Long> {
+
+    @Query("select g from UserGenre ug left join fetch ug.genre g where ug.user = :user limit 5")
+    List<Genre> findTop5GenresByUser(@Param("user") User user);
 }
