48586f2c8d8f28b000dc7024e1a029a313db1e97
Jisoo.R <jisuryu0527@gmail.com>
2024-11-25 16:39:30 +0900

    feat(playlist-controller): 플레이리스트 생성 api 추가 #4

--- src/main/java/com/my/firstbeat/web/controller/playlist/PlaylistController.java
+++ src/main/java/com/my/firstbeat/web/controller/playlist/PlaylistController.java
@@ -1,9 +1,31 @@
 package com.my.firstbeat.web.controller.playlist;
 
+import com.my.firstbeat.web.config.security.loginuser.LoginUser;
+import com.my.firstbeat.web.controller.playlist.dto.request.PlaylistCreateRequest;
+import com.my.firstbeat.web.controller.playlist.dto.response.PlaylistCreateResponse;
+import com.my.firstbeat.web.service.PlaylistService;
 import lombok.RequiredArgsConstructor;
+import org.springframework.http.ResponseEntity;
+import org.springframework.security.core.annotation.AuthenticationPrincipal;
+import org.springframework.web.bind.annotation.PostMapping;
+import org.springframework.web.bind.annotation.RequestBody;
 import org.springframework.web.bind.annotation.RestController;
 
 @RestController
 @RequiredArgsConstructor
 public class PlaylistController {
+
+
+    private final PlaylistService playlistService;
+
+    @PostMapping("/api/v1/playlists")
+    public ResponseEntity<PlaylistCreateResponse> createPlaylist(
+            @AuthenticationPrincipal LoginUser loginUser,
+            @RequestBody PlaylistCreateRequest request) {
+
+        PlaylistCreateResponse response = playlistService.createPlaylist(loginUser, request);
+        return ResponseEntity.ok(response);
+    }
+
+
 }
