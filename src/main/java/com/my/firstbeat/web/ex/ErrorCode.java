b7d790c6f089a3e7ac86791e64e9dd45536e62c9
HAEDAL-IRLAB <jyh4282002@naver.com>
2024-11-25 16:55:52 +0900

    feat(ErrorCode): NotFoundUser 에러코드 추가

--- src/main/java/com/my/firstbeat/web/ex/ErrorCode.java
+++ src/main/java/com/my/firstbeat/web/ex/ErrorCode.java
@@ -4,7 +4,7 @@ import lombok.Getter;
 
 @Getter
 public enum ErrorCode {
-    ;
+    NotFoundUser("Access denied for user data", 403); // UserId를 통해 해당 유저가 존재하는지 하지 않는지 파악하는것을 막기위해 403 코드로 적용
     private final String message;
     private final int status;
 
