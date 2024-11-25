35570be38fc68d56c92c6d84813245e7ef411dd3
HAEDAL-IRLAB <jyh4282002@naver.com>
2024-11-25 17:56:04 +0900

    feat(Genre): 테스트 코드 작성을 위해 AllArgsConstructor, Builder 어노테이션 추가

--- src/main/java/com/my/firstbeat/web/domain/genre/Genre.java
+++ src/main/java/com/my/firstbeat/web/domain/genre/Genre.java
@@ -5,14 +5,13 @@ import jakarta.persistence.Entity;
 import jakarta.persistence.GeneratedValue;
 import jakarta.persistence.GenerationType;
 import jakarta.persistence.Id;
-import lombok.AccessLevel;
-import lombok.AllArgsConstructor;
-import lombok.Getter;
-import lombok.NoArgsConstructor;
+import lombok.*;
 
 @Entity
 @NoArgsConstructor(access = AccessLevel.PROTECTED)
 @Getter
+@AllArgsConstructor
+@Builder
 public class Genre extends BaseEntity {
 
     @Id @GeneratedValue(strategy = GenerationType.IDENTITY)
