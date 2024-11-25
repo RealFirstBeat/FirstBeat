70f3efc441789da0b012cdd8eb4f70c980b17b21
ㅇi쀼z1 <yaewon0411@naver.com>
2024-11-25 11:55:40 +0900

    feat(genre-controller): 장르 기반 트랙 조회 api 쿼리 파라미터 추가 #9

--- src/main/java/com/my/firstbeat/web/controller/genre/GenreController.java
+++ src/main/java/com/my/firstbeat/web/controller/genre/GenreController.java
@@ -5,18 +5,22 @@ import com.my.firstbeat.web.service.TrackService;
 import com.my.firstbeat.web.util.api.ApiResult;
 import lombok.RequiredArgsConstructor;
 import org.springframework.http.ResponseEntity;
+import org.springframework.validation.annotation.Validated;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.RequestParam;
 import org.springframework.web.bind.annotation.RestController;
 
 @RestController
 @RequiredArgsConstructor
+@Validated
 public class GenreController {
 
     private final TrackService trackService;
 
     @GetMapping("/auth/search/track") //임시 api
-    public ResponseEntity<ApiResult<TrackSearchResponse>> searchTrackList(@RequestParam(value = "genre") String genre){
+    public ResponseEntity<ApiResult<TrackSearchResponse>> searchTrackList(@RequestParam(value = "genre", required = true) String genre,
+                                                                          @RequestParam(value = "page", required = false, defaultValue = "0") Long page,
+                                                                          @RequestParam(value = "limit", required = false, defaultValue = "20")  Long limit){
         return ResponseEntity.ok(ApiResult.success(trackService.searchTrackList(genre)));
     }
 }
