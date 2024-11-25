1cd02e0e8f3afd9a575d00c36ee4cee948789dc8
HAEDAL-IRLAB <jyh4282002@naver.com>
2024-11-25 16:11:02 +0900

    feat(UserGenreRepository): 사용자별 관심장르 조회 메서드 추가

--- src/main/java/com/my/firstbeat/web/domain/userGenre/UserGenreRepository.java
+++ src/main/java/com/my/firstbeat/web/domain/userGenre/UserGenreRepository.java
@@ -1,6 +1,8 @@
 package com.my.firstbeat.web.domain.userGenre;
 
+import java.util.List;
 import org.springframework.data.jpa.repository.JpaRepository;
 
 public interface UserGenreRepository extends JpaRepository<UserGenre, Long> {
+    List<UserGenre> findByUserId(Long userId);
 }
