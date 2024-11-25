a970145ac3b85517a35ae679eccc8ba3e5dda5e8
ㅇi쀼z1 <yaewon0411@naver.com>
2024-11-25 19:04:22 +0900

    feat(user-service): 유저 조회 기능 추가 #9

--- src/main/java/com/my/firstbeat/web/service/UserService.java
+++ src/main/java/com/my/firstbeat/web/service/UserService.java
@@ -1,5 +1,9 @@
 package com.my.firstbeat.web.service;
 
+import com.my.firstbeat.web.domain.user.User;
+import com.my.firstbeat.web.domain.user.UserRepository;
+import com.my.firstbeat.web.ex.BusinessException;
+import com.my.firstbeat.web.ex.ErrorCode;
 import lombok.RequiredArgsConstructor;
 import lombok.extern.slf4j.Slf4j;
 import org.springframework.stereotype.Service;
@@ -10,4 +14,11 @@ import org.springframework.transaction.annotation.Transactional;
 @Transactional(readOnly = true)
 @Slf4j
 public class UserService {
+
+    private final UserRepository userRepository;
+
+    public User findByIdOrFail(Long id){
+        return userRepository.findById(id)
+                .orElseThrow(() -> new BusinessException(ErrorCode.USER_NOT_FOUNT));
+    }
 }
