6f32746907d17bdfb28dd3befbfde0663013e484
Jisoo.R <jisuryu0527@gmail.com>
2024-11-25 16:41:09 +0900

    feat(playlist-service): 플레이리스트 생성 기능 추가 #4

--- src/main/java/com/my/firstbeat/web/service/PlaylistService.java
+++ src/main/java/com/my/firstbeat/web/service/PlaylistService.java
@@ -1,5 +1,11 @@
 package com.my.firstbeat.web.service;
 
+import com.my.firstbeat.web.config.security.loginuser.LoginUser;
+import com.my.firstbeat.web.controller.playlist.dto.request.PlaylistCreateRequest;
+import com.my.firstbeat.web.controller.playlist.dto.response.PlaylistCreateResponse;
+import com.my.firstbeat.web.domain.playlist.Playlist;
+import com.my.firstbeat.web.domain.playlist.PlaylistRepository;
+import com.my.firstbeat.web.domain.user.User;
 import lombok.RequiredArgsConstructor;
 import lombok.extern.slf4j.Slf4j;
 import org.springframework.stereotype.Service;
@@ -10,4 +16,22 @@ import org.springframework.transaction.annotation.Transactional;
 @Transactional(readOnly = true)
 @Slf4j
 public class PlaylistService {
+
+
+    private final PlaylistRepository playlistRepository;
+
+    public PlaylistCreateResponse createPlaylist(LoginUser loginUser, PlaylistCreateRequest request) {
+
+        User user = loginUser.getUser();
+
+        Playlist playlist = new Playlist(
+                request.getTitle(),
+                request.getDescription(),
+                user
+        );
+
+        Playlist savedPlaylist = playlistRepository.save(playlist);
+
+        return new PlaylistCreateResponse(savedPlaylist.getId(), savedPlaylist.getTitle(), savedPlaylist.getDescription());
+    }
 }
