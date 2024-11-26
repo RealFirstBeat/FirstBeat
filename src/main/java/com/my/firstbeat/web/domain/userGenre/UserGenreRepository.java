c36e896f30a3d826c5ed68235daf1a7b2599371d
HAEDAL-IRLAB <jyh4282002@naver.com>
2024-11-26 17:05:07 +0900

    rebase(feat/user/update-my-page): 충돌 해결

--- src/main/java/com/my/firstbeat/web/domain/userGenre/UserGenreRepository.java
+++ src/main/java/com/my/firstbeat/web/domain/userGenre/UserGenreRepository.java
@@ -1,14 +1,17 @@
 package com.my.firstbeat.web.domain.userGenre;
 
-import org.springframework.data.jpa.repository.JpaRepository;
-import org.springframework.data.jpa.repository.Query;
-import org.springframework.data.repository.query.Param;
-
 import java.util.List;
 
+import io.lettuce.core.dynamic.annotation.Param;
+import org.springframework.data.jpa.repository.JpaRepository;
+import org.springframework.data.jpa.repository.Modifying;
+import org.springframework.data.jpa.repository.Query;
 
 public interface UserGenreRepository extends JpaRepository<UserGenre, Long> {
     @Query("SELECT ug FROM UserGenre ug JOIN FETCH ug.genre WHERE ug.user.id = :userId")
     List<UserGenre> findByUserIdWithGenre(@Param("userId") Long userId);
 
+    @Modifying
+    @Query("DELETE FROM UserGenre ug WHERE ug.user.id = :userId")
+    void deleteByUserId(@Param("userId") Long userId);
 }
