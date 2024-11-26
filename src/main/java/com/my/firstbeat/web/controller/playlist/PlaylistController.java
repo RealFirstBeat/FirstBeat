dc7b91158b1cd14ee8e589e1383b9831d4b9860a
Jisoo.R <jisuryu0527@gmail.com>
2024-11-26 16:10:59 +0900

    feat(playlist-controller): 내가 만든 플레이리스트 조회 api 추가 #17

--- src/main/java/com/my/firstbeat/web/controller/playlist/PlaylistController.java
+++ src/main/java/com/my/firstbeat/web/controller/playlist/PlaylistController.java
@@ -3,23 +3,19 @@ package com.my.firstbeat.web.controller.playlist;
 import com.my.firstbeat.web.config.security.loginuser.LoginUser;
 import com.my.firstbeat.web.controller.playlist.dto.request.PlaylistCreateRequest;
 import com.my.firstbeat.web.controller.playlist.dto.response.PlaylistCreateResponse;
+import com.my.firstbeat.web.controller.playlist.dto.response.PlaylistRetrieveResponse;
+import com.my.firstbeat.web.domain.playlist.Playlist;
 import com.my.firstbeat.web.service.PlaylistService;
 import com.my.firstbeat.web.util.api.ApiResult;
 import jakarta.validation.Valid;
 import lombok.RequiredArgsConstructor;
+import org.springframework.data.domain.Sort;
+import org.springframework.data.web.PageableDefault;
 import org.springframework.http.ResponseEntity;
 import org.springframework.security.core.annotation.AuthenticationPrincipal;
-import org.springframework.web.bind.annotation.PostMapping;
-import org.springframework.web.bind.annotation.RequestBody;
-import org.springframework.web.bind.annotation.GetMapping;
-import org.springframework.web.bind.annotation.PathVariable;
-import org.springframework.web.bind.annotation.PutMapping;
-import org.springframework.web.bind.annotation.RequestMapping;
-import org.springframework.web.bind.annotation.RestController;
-import com.my.firstbeat.web.config.security.loginuser.LoginUser;
-import com.my.firstbeat.web.domain.playlist.Playlist;
-import com.my.firstbeat.web.service.PlaylistService;
-import com.my.firstbeat.web.util.api.ApiResult;
+import org.springframework.web.bind.annotation.*;
+import org.springframework.data.domain.Page;
+import org.springframework.data.domain.Pageable;
 
 @RestController
 @RequestMapping("/api/v1/playlist")
@@ -34,6 +30,13 @@ public class PlaylistController {
             @Valid @RequestBody PlaylistCreateRequest request) {
         return ResponseEntity.ok(ApiResult.success(playlistService.createPlaylist(loginUser.getUser(), request)));
     }
+
+	@GetMapping("/me")
+	public ResponseEntity<ApiResult<Page<PlaylistRetrieveResponse>>> getMyPlaylists(
+			@AuthenticationPrincipal LoginUser loginUser,
+			@PageableDefault(sort = "CreatedAt", direction = Sort.Direction.DESC) Pageable pageable) {
+		return ResponseEntity.ok(ApiResult.success(playlistService.getMyPlaylists(loginUser.getUser().getId(), pageable)));
+	}
 
 	// 디폴트 플레이리스트 가져오기 또는 생성
 	@GetMapping("/default")
