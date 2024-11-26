1c9b4aa74a18f07d7fb03bce52cfb2ba4c7253b7
HAEDAL-IRLAB <jyh4282002@naver.com>
2024-11-26 19:55:33 +0900

    feat(UpdateMyPageRequest): 유효성검사 dto에서 수행하도록 NotBlank, NotEmpty 어노테이션 추가

--- src/main/java/com/my/firstbeat/web/controller/user/dto/request/UpdateMyPageRequest.java
+++ src/main/java/com/my/firstbeat/web/controller/user/dto/request/UpdateMyPageRequest.java
@@ -1,16 +1,21 @@
 package com.my.firstbeat.web.controller.user.dto.request;
 
+import jakarta.validation.constraints.NotBlank;
+import jakarta.validation.constraints.NotEmpty;
 import lombok.Getter;
 import lombok.NoArgsConstructor;
 import lombok.Setter;
 
 import java.util.List;
-import java.util.Optional;
 
 @Getter
 @Setter
 @NoArgsConstructor
 public class UpdateMyPageRequest {
-    private Optional<String> name = Optional.empty();
-    private Optional<List<String>> favoriteGenre = Optional.empty();
+
+    @NotBlank(message = "Name cannot be blank")
+    private String name;
+
+    @NotEmpty(message = "Favorite genres cannot be empty")
+    private List<String> favoriteGenre;
 }
