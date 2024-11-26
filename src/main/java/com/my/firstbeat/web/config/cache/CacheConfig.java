cdd48ad88f0071eef888b934bab1037066250172
ㅇi쀼z1 <yaewon0411@naver.com>
2024-11-26 14:23:27 +0900

    feat(cache): 추천 트랙 기능에서 사용할 캐시 등록 #9

--- src/main/java/com/my/firstbeat/web/config/cache/CacheConfig.java
+++ src/main/java/com/my/firstbeat/web/config/cache/CacheConfig.java
@@ -1,2 +1,34 @@
-package com.my.firstbeat.web.config.cache;public class CacheConfig {
+package com.my.firstbeat.web.config.cache;
+
+import com.github.benmanes.caffeine.cache.Cache;
+import com.github.benmanes.caffeine.cache.Caffeine;
+import com.my.firstbeat.web.controller.track.dto.response.TrackRecommendationResponse;
+import lombok.extern.slf4j.Slf4j;
+import org.springframework.cache.CacheManager;
+import org.springframework.cache.annotation.EnableCaching;
+import org.springframework.cache.caffeine.CaffeineCacheManager;
+import org.springframework.context.annotation.Bean;
+import org.springframework.context.annotation.Configuration;
+
+import java.util.Queue;
+import java.util.concurrent.TimeUnit;
+import java.util.concurrent.locks.ReentrantLock;
+
+@Configuration
+@EnableCaching
+@Slf4j
+public class CacheConfig {
+
+    @Bean
+    public Cache<Long, Queue<TrackRecommendationResponse>> recommendationsCache()  {
+        return Caffeine.newBuilder()
+                .expireAfterWrite(24, TimeUnit.HOURS)
+                .expireAfterAccess(6, TimeUnit.HOURS)
+                .maximumSize(10000)
+                .recordStats()
+                .removalListener((key, value, cause) ->
+                        log.info("캐시 삭제 - Key: {}, Cause: {}", key, cause))
+                .build();
+    }
+
 }
