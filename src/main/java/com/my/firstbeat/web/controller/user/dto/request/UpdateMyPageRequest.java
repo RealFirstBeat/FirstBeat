6c3bd16b4ee26fef7c841904f20a0522683fadcd
HAEDAL-IRLAB <jyh4282002@naver.com>
2024-11-26 20:33:34 +0900

    feat(UserServiceTest): name 컬럼 사이즈 제한

--- src/main/java/com/my/firstbeat/web/controller/user/dto/request/UpdateMyPageRequest.java
+++ src/main/java/com/my/firstbeat/web/controller/user/dto/request/UpdateMyPageRequest.java
@@ -2,6 +2,7 @@ package com.my.firstbeat.web.controller.user.dto.request;
 
 import jakarta.validation.constraints.NotBlank;
 import jakarta.validation.constraints.NotEmpty;
+import jakarta.validation.constraints.Size;
 import lombok.Getter;
 import lombok.NoArgsConstructor;
 import lombok.Setter;
@@ -14,6 +15,7 @@ import java.util.List;
 public class UpdateMyPageRequest {
 
     @NotBlank(message = "Name cannot be blank")
+    @Size(min = 1, max = 2147483647, message = "Name must be at least 1 character")
     private String name;
 
     @NotEmpty(message = "Favorite genres cannot be empty")
