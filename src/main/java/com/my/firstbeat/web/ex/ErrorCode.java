d4bc084bada6b0448ddc1618b6f2811c0d66f430
Jisoo.R <jisuryu0527@gmail.com>
2024-11-26 16:12:58 +0900

    feat(errorcode): 내가 만든 플레이리스트 조회 기능 예외 처리용 에러코드 추가 #17

--- src/main/java/com/my/firstbeat/web/ex/ErrorCode.java
+++ src/main/java/com/my/firstbeat/web/ex/ErrorCode.java
@@ -6,8 +6,8 @@ import org.apache.hc.core5.http.HttpStatus;
 @Getter
 public enum ErrorCode {
 
-    DUPLICATE_PLAYLIST_TITLE("이미 존재하는 제목입니다.", HttpStatus.SC_CONFLICT)
-    ;
+    DUPLICATE_PLAYLIST_TITLE("이미 존재하는 제목입니다.", HttpStatus.SC_CONFLICT),
+    PLAYLIST_NOT_FOUND("플레이리스트가 존재하지 않습니다.", HttpStatus.SC_NOT_FOUND);
 
     private final String message;
     private final int status;
