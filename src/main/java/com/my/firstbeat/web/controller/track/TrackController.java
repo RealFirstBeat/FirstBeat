1ac9046fa2894b3c1be3ef6c0349bae2c5d30926
ㅇi쀼z1 <yaewon0411@naver.com>
2024-11-27 20:25:35 +0900

    feat(track-controller): 레디스를 사용한 추천 트랙 반환 api 추가 #26

--- src/main/java/com/my/firstbeat/web/controller/track/TrackController.java
+++ src/main/java/com/my/firstbeat/web/controller/track/TrackController.java
@@ -3,6 +3,7 @@ package com.my.firstbeat.web.controller.track;
 import com.my.firstbeat.web.config.security.loginuser.LoginUser;
 import com.my.firstbeat.web.controller.track.dto.response.TrackRecommendationResponse;
 import com.my.firstbeat.web.service.recommemdation.RecommendationService;
+import com.my.firstbeat.web.service.recommemdation.RecommendationServiceWithRedis;
 import com.my.firstbeat.web.util.api.ApiResult;
 import lombok.RequiredArgsConstructor;
 import org.springframework.http.ResponseEntity;
@@ -13,14 +14,21 @@ import org.springframework.web.bind.annotation.RestController;
 
 @RestController
 @RequiredArgsConstructor
-@RequestMapping("/api/v1")
+@RequestMapping("/api")
 public class TrackController {
 
     private final RecommendationService recommendationService;
+    private final RecommendationServiceWithRedis recommendationServiceWithRedis;
 
-    @GetMapping("/tracks/recommendations")
-    public ResponseEntity<ApiResult<TrackRecommendationResponse>> getRecommendations(
+    @GetMapping("/v2/tracks/recommendations")
+    public ResponseEntity<ApiResult<TrackRecommendationResponse>> getRecommendationsV2(
             @AuthenticationPrincipal LoginUser loginUser){
         return ResponseEntity.ok(ApiResult.success(recommendationService.getRecommendations(loginUser.getUser().getId())));
+    }
+
+    @GetMapping("/v3/tracks/recommendations")
+    public ResponseEntity<ApiResult<TrackRecommendationResponse>> getRecommendationV3(
+            @AuthenticationPrincipal LoginUser loginUser) {
+        return ResponseEntity.ok(ApiResult.success(recommendationServiceWithRedis.getRecommendations(loginUser.getUser().getId())));
     }
 }
