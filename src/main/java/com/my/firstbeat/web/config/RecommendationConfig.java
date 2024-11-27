9771a2278959328ab5f6b668deeff29a1bf5e6b9
ㅇi쀼z1 <yaewon0411@naver.com>
2024-11-27 17:14:35 +0900

    feat(recommendation-config): lockpropertis 추가 및 설정 #26

--- src/main/java/com/my/firstbeat/web/config/RecommendationConfig.java
+++ src/main/java/com/my/firstbeat/web/config/RecommendationConfig.java
@@ -1,10 +1,12 @@
 package com.my.firstbeat.web.config;
 
+import com.my.firstbeat.web.service.recommemdation.property.LockProperties;
 import com.my.firstbeat.web.service.recommemdation.property.RecommendationProperties;
 import org.springframework.boot.context.properties.EnableConfigurationProperties;
+import org.springframework.cache.annotation.EnableCaching;
 import org.springframework.context.annotation.Configuration;
 
 @Configuration
-@EnableConfigurationProperties(RecommendationProperties.class)
+@EnableConfigurationProperties(value = {RecommendationProperties.class, LockProperties.class})
 public class RecommendationConfig {
 }
--- src/main/java/com/my/firstbeat/web/service/recommemdation/property/LockProperties.java
+++ src/main/java/com/my/firstbeat/web/service/recommemdation/property/LockProperties.java
@@ -1,4 +1,13 @@
 package com.my.firstbeat.web.service.recommemdation.property;
 
+import lombok.Getter;
+import org.springframework.boot.context.properties.ConfigurationProperties;
+
+@ConfigurationProperties(prefix = "lock")
+@Getter
 public class LockProperties {
+
+    private String keyPrefix;
+    private int waitTime;
+    private int leaseTime;
 }
