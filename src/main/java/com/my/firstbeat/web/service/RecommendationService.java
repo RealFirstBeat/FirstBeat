e082b9c9d9862151ea22d0d5acef234a601bcb95
jangms3 <github.com/jangms3>
2024-11-27 18:05:06 +0900

    feat: 추천 트렉 제거 로직 구현  #15
    - 특정 유저의 추천에서 해당하는 트렉을 제거하는 메서드
    - 반환 값으로 제거 성공 여부를 리턴

--- src/main/java/com/my/firstbeat/web/service/RecommendationService.java
+++ src/main/java/com/my/firstbeat/web/service/RecommendationService.java
@@ -182,4 +182,14 @@ public class RecommendationService {
                 .map(Track::getSpotifyTrackId)
                 .collect(Collectors.joining(","));
     }
+
+	public boolean removeTrackFromRecommendations(Long userId, Long spotifyTrackId) {
+        Queue<TrackRecommendationResponse> recommendations =
+            recommendationsCache.get(userId, key -> new ConcurrentLinkedQueue<>());
+
+        // 큐에서 해당 트랙 제거
+        return recommendations.removeIf(
+            recommendation -> recommendation.getSpotifyTrackId().equals(String.valueOf(spotifyTrackId))
+        );
+    }
 }
