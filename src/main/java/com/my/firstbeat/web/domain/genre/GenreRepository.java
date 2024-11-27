e00cce8606c04396d684d47e783d60d12dfa0e30
HAEDAL-IRLAB <jyh4282002@naver.com>
2024-11-27 11:12:55 +0900

    rebase(feat/user/update-my-page): 충돌 해결

--- src/main/java/com/my/firstbeat/web/domain/genre/GenreRepository.java
+++ src/main/java/com/my/firstbeat/web/domain/genre/GenreRepository.java
@@ -1,14 +1,17 @@
 package com.my.firstbeat.web.domain.genre;
 
 import com.my.firstbeat.web.domain.user.User;
+import io.lettuce.core.dynamic.annotation.Param;
 import org.springframework.data.domain.Pageable;
 import org.springframework.data.jpa.repository.JpaRepository;
 import org.springframework.data.jpa.repository.Query;
-import org.springframework.data.repository.query.Param;
 
+import java.util.Set;
 import java.util.List;
 
 public interface GenreRepository extends JpaRepository<Genre, Long> {
+    @Query("SELECT g.id FROM Genre g WHERE g.name IN :names")
+    Set<Long> findIdsByNames(@Param("names") Set<String> names);
 
     @Query("select g from Genre g join UserGenre ug on ug.genre = g where ug.user = :user")
     List<Genre> findTop5GenresByUser(@Param("user") User user, Pageable pageable);
