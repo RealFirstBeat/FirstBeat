f222f846262a0696a1d25e4776b0ed49d7d00824
ㅇi쀼z1 <yaewon0411@naver.com>
2024-11-27 19:26:59 +0900

    feat(lock-properties): 백그라운드 추천 갱신용 변수 추가 #26

--- src/main/java/com/my/firstbeat/web/service/recommemdation/property/LockProperties.java
+++ src/main/java/com/my/firstbeat/web/service/recommemdation/property/LockProperties.java
@@ -1,6 +1,7 @@
 package com.my.firstbeat.web.service.recommemdation.property;
 
 import lombok.Getter;
+import lombok.Setter;
 import org.springframework.boot.context.properties.ConfigurationProperties;
 
 @ConfigurationProperties(prefix = "lock")
@@ -10,4 +11,16 @@ public class LockProperties {
     private String keyPrefix;
     private int waitTime;
     private int leaseTime;
+    private int maxAttempts;
+    private long initialBackOffMs;
+    private double backOffMultiplier;
+    private BackgroundLock background = new BackgroundLock();
+
+    @Getter
+    @Setter
+    public static class BackgroundLock {
+        private int maxAttempts = 2;
+        private long initialBackOffMs = 500;
+        private double backOffMultiplier = 2.0;
+    }
 }
