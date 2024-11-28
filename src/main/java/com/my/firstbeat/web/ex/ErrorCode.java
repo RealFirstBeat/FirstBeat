b4bf68aa4af915b816370fdc3df944ae5fba03d7
matino0216 <woqhr33@naver.com>
2024-11-28 10:18:07 +0900

    회원가입 및 이메일, 장르 유효성 테스트
    - rebase 통해 충돌 해결

--- src/main/java/com/my/firstbeat/web/ex/ErrorCode.java
+++ src/main/java/com/my/firstbeat/web/ex/ErrorCode.java
@@ -12,11 +12,10 @@ public enum ErrorCode {
     NO_NEW_RECOMMENDATIONS_AVAILABLE("현재 추천 가능한 새로운 곡이 없습니다. 잠시 후 다시 시도해주세요", HttpStatus.NOT_FOUND.value()),
     DUPLICATE_PLAYLIST_TITLE("이미 존재하는 제목입니다.", HttpStatus.CONFLICT.value()),
     MAX_RECOMMENDATION_ATTEMPTS_EXCEED("일시적으로 추천 서비스를 이용할 수 없습니다. 잠시 후 다시 시도해주세요", HttpStatus.CONFLICT.value()),
-
     PLAYLIST_NOT_FOUND("플레이리스트가 존재하지 않습니다.", HttpStatus.NOT_FOUND.value()),
-
+    DUPLICATE_EMAIL("중복된 Email 입니다.", HttpStatus.CONFLICT.value()),
+    INVALID_GENRES("유효하지 않은 장르가 포함되어 있습니다.", HttpStatus.BAD_REQUEST.value()),
     SERVICE_TEMPORARY_UNAVAILABLE("추천 서비스가 일시적으로 혼잡합니다. 잠시 후 다시 시도해주세요", HttpStatus.SERVICE_UNAVAILABLE.value()),
-
     TRACK_FETCH_ERROR("플레이리스트 내 트랙 목록을 불러오는 중 오류가 발생했습니다", HttpStatus.INTERNAL_SERVER_ERROR.value());
 
     private final String message;
