485c4bac869f0972afea19d4f3e43542fb018edc
HAEDAL-IRLAB <jyh4282002@naver.com>
2024-11-28 15:56:57 +0900

    refactor(): 불필요 import문 정리

--- src/main/java/com/my/firstbeat/web/controller/playlist/dto/response/PaginationInfo.java
+++ src/main/java/com/my/firstbeat/web/controller/playlist/dto/response/PaginationInfo.java
@@ -2,7 +2,6 @@ package com.my.firstbeat.web.controller.playlist.dto.response;
 
 import lombok.AllArgsConstructor;
 import lombok.Getter;
-import lombok.NoArgsConstructor;
 
 @Getter
 @AllArgsConstructor
